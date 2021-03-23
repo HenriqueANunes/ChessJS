bcc83474e34f7b2a9470a962df6fb6b267c9a0ff
HenriqueANunes <henrique.a.nunes@outlook.com>
2021-03-23 17:22:13 -0300

    Initial commit

---
+++ js/Main.js
@@ -0,0 +1,11 @@
+// function init(){
+//     console.log("init() called");
+// }
+var chessBoard = document.getElementById("chessBoard");
+
+for (var i=0; i<8; i++){
+    var row = chessBoard.appendChild(document.createElement("div"));
+    for (var j=0; j<8; j++){
+        row.appendChild(document.createElement("span"));
+    }
+}
